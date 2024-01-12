6cd8b525ee295d2c80da9e1bd62ce7e6a3aff3cd
pablonq <pablo.navarro@est.fi.uncoma.edu.ar>
2024-01-12 12:33:32 -0300

    modificando estilo

--- Vista/Estructuras/footer.php
+++ Vista/Estructuras/footer.php
@@ -1,28 +1,24 @@
 
 <!-- Footer -->
-<footer class="text-center text-white" style="background-color: #0a4275;">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-      <!-- Section: CTA -->
-      <section class="">
-      <i class="bi bi-geo-alt"></i>
-      <i class="bi bi-whatsapp"></i>
-      <i class="bi bi-instagram"></i>
-        <p class="d-flex justify-content-center align-items-center">
-        
-        </p>
-      </section>
-      <!-- Section: CTA -->
-    </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    <a href="https://mdbootstrap.com/"><i class="bi bi-github"></i></a>
-      Pablo Navarro FAI - 4284
+    <footer class="text-center text-white" style="background-color: #0a4275;">
+      <!-- Grid container -->
+        <div class="footer_contacto p-5 pb-4 text-center">
+          <!-- Section: CTA -->
+          
+          <a href="https://maps.app.goo.gl/S6XNcDTyC24CSKoK6"><i class="icon bi bi-geo-alt"></i></a>
+          Int. Chaneton 1620
+          <a href="https://github.com/pablonq/PWD-FINAL"><i class="icon bi bi-whatsapp"></i></a>
+          299-1234567
+          <a href="https://www.instagram.com/"><i class="icon bi bi-instagram"></i></a>
+          @ferreteria_chaneton
+            
+          
+        </div>
+        <div class="footer_contacto text-center p-3 footer_git" style="background-color: rgba(0, 0, 0, 0.2);">
+          <a href="https://github.com/pablonq/PWD-FINAL"><i class="icon bi bi-github"></i></a>
+          Pablo Navarro FAI - 4284
+        </div>
       
-    </div>
-    <!-- Copyright -->
-  </footer>
-</body>
+    </footer>
+  </body>
 </html>
