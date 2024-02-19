01a2ba037eba23e52a3ea598ec3d3129f35bf57a
Andres Quezada <andresquezada@Pablo-Navarro.local>
2024-02-19 09:24:06 -0300

    ultimo

--- Vista/Estructuras/navInseguro.php
+++ Vista/Estructuras/navInseguro.php
@@ -6,7 +6,7 @@ $direccionPadre = $session->getDireccionPadreMenu();
 ?>
 
 <!-- ________________________________________ NAV INSEGURO _____________________________________ -->
-<nav class="navbar navbar-expand-sm">
+<nav class="navbar navbar-expand-lg">
     <div class="container">
     <a href="home.php"> <img src="../img/logo.png" class="logo" alt="Logo"></a>
         <a class="navbar-brand text-white marca" href="#">FERRETERIA CHANETON</a>
